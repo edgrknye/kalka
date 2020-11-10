359f5f1eb6b8eff23463d51ce9e3cdd6bf9995d7
Edgar Kanyesigye <edgrknye@gmail.com>
2020-11-10 09:54:36 +0300

    render form

--- lib/kalka_web/live/link_live.ex
+++ lib/kalka_web/live/link_live.ex
@@ -1,25 +1,36 @@
 defmodule KalkaWeb.LinkLive do
   use KalkaWeb, :live_view
 
+  use Phoenix.HTML
+  import KalkaWeb.ErrorHelpers
+
+  alias Kalka.Shortener
+  alias Kalka.Shortener.Link
+  require Logger
+
   def mount(_params, _session, socket) do
+    socket =
+      socket
+      |> assign(:changeset, Shortener.change_link(%Link{}))
+      |> assign(:shortlink, "")
+
     {:ok, socket}
+    # {:ok, assign(socket, %{changeset: Shortener.change_link(%Link{}), shortlink: ""})}
   end
 
-  # def render(assigns) do
-  #   ~L"""
-  #   <% f = form_for @changeset, "#", [phx_change: :validate, phx_submit: :save] %>
-  #    <%= label f, :url %>
-  #    <%= text_input f, :url %>
-  #    <%= error_tag f, :url %>
+  def handle_event("validate", %{"link" => link_params}, socket) do
+    changeset =
+      %Link{}
+      |> Shortener.change_link(link_params)
+      |> Map.put(:action, :insert)
 
-  #    <%= submit "Generate" %>
-  #   </form>
-  #   """
-  # end
+    {:noreply, assign(socket, changeset: changeset)}
+  end
 
-  def render(assigns) do
-    ~L"""
-    Phoenix is awesome
-    """
+  def handle_event("create_link", %{"link" => link_params}, socket) do
+    case Shortener.create_link(link_params) do
+      {:ok, link} -> {:noreply, assign(socket, :shortlink, link.slug)}
+      {:error, %Ecto.Changeset{} = changeset} -> {:noreply, assign(socket, changeset: changeset)}
+    end
   end
 end
