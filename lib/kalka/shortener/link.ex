cd4109d7a20ee2655c66b8951d694774fd17155b
Edgar Kanyesigye <edgrknye@gmail.com>
2020-11-10 08:09:45 +0300

    validate slug and url

--- lib/kalka/shortener/link.ex
+++ lib/kalka/shortener/link.ex
@@ -13,6 +13,12 @@ defmodule Kalka.Shortener.Link do
   def changeset(link, attrs) do
     link
     |> cast(attrs, [:url, :slug])
-    |> validate_required([:url, :slug])
+    |> validate_required([:url])
+    |> validate_format(:slug, ~r/\A[-a-zA-Z0-9_]+\z/,
+      message: "can only contain only numbers, letters, dashes and underscores"
+    )
+    |> validate_format(:url, UrlHelper.regex(), message: "invalid URL")
+    |> validate_length(:slug, min: 3, max: 10)
+    |> unique_constraint(:slug)
   end
 end
